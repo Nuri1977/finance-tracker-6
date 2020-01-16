ec143a6fd1f4c4a982eb04143de022854abb6b7b
nuri1977 <nurilacka@gmail.com>
2020-01-16 02:28:20 +0100

    USe Ajax for form submision

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,14 +4,20 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @akcija = Stock.new_lookup(params[:stock])
       if @akcija
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result'}
+        end
       else
-        flash[:alert] = "Please enter a valid symbol search"
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol search"
+          format.js { render partial: 'users/result'}
+        end
       end
     else
-      flash[:alert] = "Please enter a symbol to search"
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol search"
+        format.js { render partial: 'users/result'}
+      end
     end
   end
 
